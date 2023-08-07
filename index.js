95b2a8255c7ceaa95f5a6a0ed713b13ad0c1b03a
Nathan Welliver <nwelliver.564@gmail.com>
2023-08-07 15:02:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+
+    const paragraphElement = document.querySelector('p');
+    paragraphElement.textContent = "This is really cool!" ;
+});
